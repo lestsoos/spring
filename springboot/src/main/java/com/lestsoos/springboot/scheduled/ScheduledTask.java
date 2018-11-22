ac5d113492eb846e26af518b426a98ab459042ad
lestsoos <lestsoos@163.com>
2018-11-23 00:52:40 +0800

    init springboot

--- springboot/src/main/java/com/lestsoos/springboot/scheduled/ScheduledTask.java
+++ springboot/src/main/java/com/lestsoos/springboot/scheduled/ScheduledTask.java
@@ -1,7 +1,6 @@
 package com.lestsoos.springboot.scheduled;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -17,20 +16,19 @@ import java.util.Date;
  * （7）initialDelayString：与initialDelay的含义一样，只是将参数类型变为String；
  * （8）zone：时区，默认为当前时区，一般没有用到。
  */
-
+@Slf4j
 @Component
 public class ScheduledTask {
-    private Logger logger = LoggerFactory.getLogger(ScheduledTask.class);
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     @Scheduled(fixedDelayString = "${scheduled.jobs.fixedDelay}")
     public void getTask1() {
-        logger.info("任务1,从配置文件加载任务信息，当前时间：" + dateFormat.format(new Date()));
+        log.info("任务1,从配置文件加载任务信息，当前时间：" + dateFormat.format(new Date()));
     }
 
     @Scheduled(cron = "${scheduled.jobs.cron}")
     public void getTask2() {
-        logger.info("任务2,从配置文件加载任务信息，当前时间：" + dateFormat.format(new Date()));
+        log.info("任务2,从配置文件加载任务信息，当前时间：" + dateFormat.format(new Date()));
     }
 
     private int fixedDelayCount = 1;
@@ -40,21 +38,21 @@ public class ScheduledTask {
 
      @Scheduled(fixedDelay = 5000)        //fixedDelay = 5000表示当前方法执行完毕5000ms后，Spring scheduling会再次调用该方法
     public void testFixDelay() {
-        logger.info("===fixedDelay: 第{}次执行方法", fixedDelayCount++);
+        log.info("===fixedDelay: 第{}次执行方法", fixedDelayCount++);
     }
 
      @Scheduled(fixedRate = 5000)        //fixedRate = 5000表示当前方法开始执行5000ms后，Spring scheduling会再次调用该方法
     public void testFixedRate() {
-        logger.info("===fixedRate: 第{}次执行方法", fixedRateCount++);
+        log.info("===fixedRate: 第{}次执行方法", fixedRateCount++);
     }
 
     @Scheduled(initialDelay = 1000, fixedRate = 5000)   //initialDelay = 1000表示延迟1000ms执行第一次任务
     public void testInitialDelay() {
-        logger.info("===initialDelay: 第{}次执行方法", initialDelayCount++);
+        log.error("===initialDelay: 第{}次执行方法", initialDelayCount++);
     }
 
     @Scheduled(cron = "0 0/1 * * * ?")  //cron接受cron表达式，根据cron表达式确定定时规则
     public void testCron() {
-        logger.info("===initialDelay: 第{}次执行方法", cronCount++);
+        log.warn("===initialDelay: 第{}次执行方法", cronCount++);
     }
 }
