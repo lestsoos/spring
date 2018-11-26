cb154d511e153f2f5b3f43048c95c37dd38ffd3d
lestsoos <lestsoos@163.com>
2018-11-26 16:57:19 +0800

    first three

--- springboot/src/main/java/com/lestsoos/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/lestsoos/springboot/SpringbootApplication.java
@@ -2,9 +2,13 @@ package com.lestsoos.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@EnableScheduling
+//@EnableScheduling
+
+@ServletComponentScan
 @SpringBootApplication
 public class SpringbootApplication {
 
---
+++ springboot/src/main/java/com/lestsoos/springboot/redis/session/RedisSessionConfig.java
@@ -0,0 +1,9 @@
+package com.lestsoos.springboot.redis.session;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisSessionConfig {
+}
